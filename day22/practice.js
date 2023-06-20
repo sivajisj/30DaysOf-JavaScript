51ae40b9735993cbd0fcc6d9182c19ab738d004b
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2023-06-20 19:27:41 +0530

    day 22 is uploaded

---
+++ day22/practice.js
@@ -0,0 +1,12 @@
+const ul = document.querySelector('ul')
+// const lists = document.querySelectorAll('li')
+// for(const a of lists){
+//     ul.removeChild(a);
+// }
+// const ul = document.('ul')
+//         const lists = document.querySelectorAll('li')
+//         for (let list of lists) {
+//             ul.removeChild(list)
+
+//         }
+ul.innerHTML='';
